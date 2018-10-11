1a8f5ec8837e95109c5b4db03772497a5acc989b
Shin Dongjin <djshin2000@gmail.com>
2018-10-11 18:17:42 +0900

    master Modify 잘못 올라간 파일 삭제

--- chapter_11/sample.py
+++
@@ -1,7 +0,0 @@
-# add to stores/views.py
-from django.views.generic import ListView
-from .models import Store
-from core.views import TitleSearchMixin
-
-class IceCreamStoreListView(TitleSearchMixin, ListView):
-    model = Store
